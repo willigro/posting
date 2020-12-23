e33797e41671bcfc0e085b78f44ee51a9293c2a5
Rittmann <willirittmann@outlook.com>
2020-12-22 23:23:15 -0300

    Generic execute async

--- app/src/main/java/com/rittmann/posting/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/rittmann/posting/ui/base/BaseViewModel.kt
@@ -3,34 +3,45 @@ package com.rittmann.posting.ui.base
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 abstract class BaseViewModel : ViewModel() {
 
+    protected var viewModelScopeGen: CoroutineScope? = null
     private val _progress = MutableLiveData<Boolean>()
 
     val progress: LiveData<Boolean>
         get() = _progress
 
-    /**
-     *
-     * By marking `block` as `suspend` this creates a suspend lambda which can call suspend
-     * functions.
-     *
-     * @param block lambda to actually load data. It is called in the viewModelScope. Before calling the
-     *              lambda the loading spinner will display, after completion or error the loading
-     *              spinner will stop
-     */
-    fun executeAsync(dispatcher: CoroutineDispatcher = Dispatchers.IO, block: suspend () -> Unit) {
-        viewModelScope.launch {
+    fun executeAsyncProgress(
+        dispatcher: CoroutineDispatcher = Dispatchers.IO,
+        scope: CoroutineScope = GlobalScope,
+        block: suspend () -> Unit
+    ) {
+        val s = viewModelScopeGen ?: scope
+        s.launch {
             withContext(dispatcher) {
                 _progress.postValue(true)
                 block()
                 _progress.postValue(false)
+            }
+        }
+    }
+
+    fun executeAsync(
+        dispatcher: CoroutineDispatcher = Dispatchers.IO,
+        scope: CoroutineScope = GlobalScope,
+        block: suspend () -> Unit
+    ) {
+        val s = viewModelScopeGen ?: scope
+        s.launch {
+            withContext(dispatcher) {
+                block()
             }
         }
     }
